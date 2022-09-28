72761272f5c7cd108b4349db44bfa424c070f0ad
xoma_star <sasha.sirotyin09@gmail.com>
2022-09-28 13:07:49 +0500

    skip no screens

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const client = new PocketBase('https://pb.xoma-star.tk');
 
 
 async function main(){
-    for(let i = 100000; i < 200000; i++){
+    for(let i = 0; i < 1000000; i++){
         try{
             const movie = (await axios.get(`https://api.themoviedb.org/3/movie/${i}`, {params: {api_key: 'c8ae8a04674ddb8d64d4cb06205be86d', language: 'ru-RU'}})).data
             if(movie){
                 movie.screens = (await axios.get(`https://api.themoviedb.org/3/movie/${movie.id}/images?include_image_language=en,null`, {params: {api_key: 'c8ae8a04674ddb8d64d4cb06205be86d'}})).data.backdrops
+                if(movie.screens.length < 1) continue
                 const toPaste = {
                     imdb_id: movie.imdb_id,
                     tmdb_id: movie.id,
@@ -23,10 +24,12 @@ async function main(){
                     companies: movie.production_companies.map(x => x.name),
                     countries: movie.production_countries.map(x => x.name),
                     rating: movie.vote_average,
-                    budget: movie.budget
+                    budget: movie.budget,
+                    popularity: movie.popularity,
+                    runtime: movie.runtime
                 }
                 // console.log(toPaste)
-                if(toPaste.screens.length > 0) await client.records.create('movies', toPaste)
+                await client.records.create('movies', toPaste)
                 console.log(`pasted ${movie.title} into Pocket`)
             }
         }catch (e) {}
